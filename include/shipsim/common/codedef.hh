cd927bc2ed515b31a16245d7fc0ab807e1a3c0f1
EccentricTeal <suisei_wada@naoe.eng.osaka-u.ac.jp>
2021-01-17 16:41:50 +0900

    Backup

---
+++ include/shipsim/common/codedef.hh
@@ -0,0 +1,26 @@
+#ifndef SHIPSIM_ERROR_HH
+#define SHIPSIM_ERROR_HH
+
+namespace shipsim::common
+{
+
+    enum class Status: int
+    {
+        READY = 0,
+        RUNNING = 1,
+        NOT_READY = -1,
+        ERROR = -2
+    };
+
+    enum class Error: int
+    {
+        OK = 0,
+        FAILED_TO_RUN = -1,
+        INVALID_MODEL = -10,
+        DIV_BY_ZERO = -20
+    };    
+    
+}
+
+
+#endif
